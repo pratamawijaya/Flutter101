7874a813766c91e4c30ad092ca84a0271a6d522b
Pratama Nur Wijaya <set.mnemonix@gmail.com>
2019-06-15 17:33:10 +0700

    update readme update demo gridview

---
+++ README.md
@@ -0,0 +1,13 @@
+# Flutter Learning Tracks
+
+
+## 01 Learn how flutter work
+- how to use widget
+- how to use library at pubspec.yml
+- how to create listview
+- how to add click listener
+
+## 02, learn how to create complex ui
+- how to use import image with flutter and display it
+- introduction to column, and row
+
--- flutter_03_layout_gallery/lib/main.dart
+++ flutter_03_layout_gallery/lib/main.dart
@@ -6,40 +6,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    var container = Container(
-      decoration: BoxDecoration(
-        color: Colors.black26
-      ),
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 10.0, color: Colors.black38),
-                    borderRadius: const BorderRadius.all(const Radius.circular(8.0))
-                  ),
-                  margin: const EdgeInsets.all(4.0),
-                  child: Image.asset('images/dog4.png'),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 10.0, color: Colors.black38),
-                    borderRadius: const BorderRadius.all(const Radius.circular(8.0))
-                  ),
-                  margin: const EdgeInsets.all(4.0),
-                  child: Image.asset('images/dog4.png'),
-                ),
-              )
-            ],
-          )
-        ],
-      ),
+    var anjing = Column(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+              border: Border.all(width: 5.0, color: Colors.black12),
+              borderRadius: const BorderRadius.all(const Radius.circular(8.0))),
+          margin: const EdgeInsets.all(4.0),
+          child: Image.asset('images/dog4.png'),
+        )
+      ],
     );
+
+    var gridView = GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(100, (index) {
+          return anjing;
+        }));
 
     return MaterialApp(
       title: 'Demo',
@@ -47,7 +30,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Container Layout'),
         ),
-        body: container,
+        body: gridView,
       ),
     );
   }
