297a0b3504bad401005262aef7a3245cb6d1385a
Pratama Nur Wijaya <set.mnemonix@gmail.com>
2019-06-17 13:12:29 +0700

    init project sampleapp state

---
+++ flutter_12_sample_appstate_withprovider/android/app/src/main/java/com/example/flutter_12_sample_appstate_withprovider/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.flutter_12_sample_appstate_withprovider;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
