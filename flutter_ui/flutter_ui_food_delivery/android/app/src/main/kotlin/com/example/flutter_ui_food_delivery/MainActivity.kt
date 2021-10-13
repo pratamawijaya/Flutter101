436380d26c9bd2d3a846451bebd00ae0779ad81c
pratamawijaya <set.mnemonix@gmail.com>
2021-10-13 08:18:35 +0700

    init project ui food delivery

---
+++ flutter_ui/flutter_ui_food_delivery/android/app/src/main/kotlin/com/example/flutter_ui_food_delivery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_ui_food_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_ui/flutter_ui_food_delivery/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_food_delivery/screens/home/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Food Delivery',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
