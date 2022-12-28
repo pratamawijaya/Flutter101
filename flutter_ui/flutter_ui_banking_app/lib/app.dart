78fe65282cea07d7f602b15f8bb77384e0458b8d
pratamawijaya <set.mnemonix@gmail.com>
2022-12-28 19:41:14 +0700

    optimize import

--- flutter_ui/flutter_ui_banking_app/lib/app.dart
+++ flutter_ui/flutter_ui_banking_app/lib/app.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_ui_banking_app/configs/themes.dart';
 import 'package:flutter_ui_banking_app/routes.dart';
 
